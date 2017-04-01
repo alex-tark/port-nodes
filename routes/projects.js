526a23de7754e1a1e8c1a9992309f0ff8db85c94
nazicrim <nazicrim@yandex.ru>
2017-04-01 04:54:51 +0300

    3

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.query.id == undefined) {
+    model.project.find()
+    res.render('index', { title: 'Project list' });
+  }
+  else {
+    model.project.findOne(req.query.id).then(function (document) {
+      if (document == null) res.redirect('/project');
+      else res.json(document);
+    });
+  }
+});
+
+router.post('/project', function (req, res) {
+  project.add(req.body, function (document) {
+    res.json(document);
+  });
 });
 
 module.exports = router;
