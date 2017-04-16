db15d086a1a31d850f44c23fe145ee7d80ed2e12
nazicrim <nazicrim@yandex.ru>
2017-04-16 16:32:18 +0300

    fix some small bugs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Project code' });
 });
 
+router.get('/start', function(req, res, next) {
+  res.render('helpers/start', { title: "Get started with Project manager | Project code" });
+});
+
 module.exports = router;
